657d9c9be8423b3f7427a8a749503f8f3a86982b
Matthew Powelson <powelson.matthew@gmail.com>
2020-04-01 16:14:26 -0500

    Add file licenses

--- src/dense_inversion.cpp
+++ src/dense_inversion.cpp
@@ -1,6 +1,28 @@
 /**
- * Benchmarks the inversion of 1 random square matrix of increasing size
- **/
+ * @file dense_inversion.cpp
+ * @brief Benchmarks the inversion of 1 random square matrix of increasing size
+ *
+ * @author Matthew Powelson
+ * @date April 1, 2020
+ * @version TODO
+ * @bug No known bugs
+ *
+ * @copyright Copyright (c) 2020, Southwest Research Institute
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include <benchmark/benchmark.h>
 
@@ -50,32 +72,44 @@ int main(int argc, char** argv)
   for (auto& test_input : { 2, 3, 4, 8, 16, 32, 64, 128, 256, 512 })
   {
     std::string name = "BM_PYTORCH_CPU_INV_Size_" + std::to_string(test_input);
-    benchmark::RegisterBenchmark(name.c_str(), BM_PYTORCH_INV, test_input, torch::kCPU)->UseRealTime()->Unit(benchmark::TimeUnit::kMicrosecond);
+    benchmark::RegisterBenchmark(name.c_str(), BM_PYTORCH_INV, test_input, torch::kCPU)
+        ->UseRealTime()
+        ->Unit(benchmark::TimeUnit::kMicrosecond);
   }
   for (auto& test_input : { 2, 3, 4, 8, 16, 32, 64, 128, 256, 512 })
   {
     std::string name = "BM_PYTORCH_GPU_INV_Size_" + std::to_string(test_input);
-    benchmark::RegisterBenchmark(name.c_str(), BM_PYTORCH_INV, test_input, torch::kCUDA)->UseRealTime()->Unit(benchmark::TimeUnit::kMicrosecond);
+    benchmark::RegisterBenchmark(name.c_str(), BM_PYTORCH_INV, test_input, torch::kCUDA)
+        ->UseRealTime()
+        ->Unit(benchmark::TimeUnit::kMicrosecond);
   }
   for (auto& test_input : { 2, 3, 4, 8, 16, 32, 64, 128, 256, 512 })
   {
     std::string name = "BM_EIGEN_INV_Size_" + std::to_string(test_input);
-    benchmark::RegisterBenchmark(name.c_str(), BM_EIGEN_INV, test_input)->UseRealTime()->Unit(benchmark::TimeUnit::kMicrosecond);
+    benchmark::RegisterBenchmark(name.c_str(), BM_EIGEN_INV, test_input)
+        ->UseRealTime()
+        ->Unit(benchmark::TimeUnit::kMicrosecond);
   }
   for (auto& test_input : { 2, 3, 4, 8, 16, 32, 64, 128, 256, 512 })
   {
     std::string name = "BM_ARRAYFIRE_CPU_INV_Size_" + std::to_string(test_input);
-    benchmark::RegisterBenchmark(name.c_str(), BM_ARRAYFIRE_INV, test_input, AF_BACKEND_CPU)->UseRealTime()->Unit(benchmark::TimeUnit::kMicrosecond);
+    benchmark::RegisterBenchmark(name.c_str(), BM_ARRAYFIRE_INV, test_input, AF_BACKEND_CPU)
+        ->UseRealTime()
+        ->Unit(benchmark::TimeUnit::kMicrosecond);
   }
   for (auto& test_input : { 2, 3, 4, 8, 16, 32, 64, 128, 256, 512 })
   {
     std::string name = "BM_ARRAYFIRE_CUDA_INV_Size_" + std::to_string(test_input);
-    benchmark::RegisterBenchmark(name.c_str(), BM_ARRAYFIRE_INV, test_input, AF_BACKEND_CUDA)->UseRealTime()->Unit(benchmark::TimeUnit::kMicrosecond);
+    benchmark::RegisterBenchmark(name.c_str(), BM_ARRAYFIRE_INV, test_input, AF_BACKEND_CUDA)
+        ->UseRealTime()
+        ->Unit(benchmark::TimeUnit::kMicrosecond);
   }
   for (auto& test_input : { 2, 3, 4, 8, 16, 32, 64, 128, 256, 512 })
   {
     std::string name = "BM_ARRAYFIRE_OPENCL_INV_Size_" + std::to_string(test_input);
-    benchmark::RegisterBenchmark(name.c_str(), BM_ARRAYFIRE_INV, test_input, AF_BACKEND_OPENCL)->UseRealTime()->Unit(benchmark::TimeUnit::kMicrosecond);
+    benchmark::RegisterBenchmark(name.c_str(), BM_ARRAYFIRE_INV, test_input, AF_BACKEND_OPENCL)
+        ->UseRealTime()
+        ->Unit(benchmark::TimeUnit::kMicrosecond);
   }
   benchmark::Initialize(&argc, argv);
   benchmark::RunSpecifiedBenchmarks();
--- src/dense_multiplication.cpp
+++ src/dense_multiplication.cpp
@@ -1,6 +1,28 @@
 /**
- * Benchmarks the multiplication of 2 random square matrices of increasing size
- **/
+ * @file dense_multiplication.cpp
+ * @brief Benchmarks the multiplication of 2 random square matrices of increasing size
+ *
+ * @author Matthew Powelson
+ * @date April 1, 2020
+ * @version TODO
+ * @bug No known bugs
+ *
+ * @copyright Copyright (c) 2020, Southwest Research Institute
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include <benchmark/benchmark.h>
 
@@ -53,32 +75,44 @@ int main(int argc, char** argv)
   for (auto& test_input : { 2, 3, 4, 8, 16, 32, 64, 128, 256, 512 })
   {
     std::string name = "BM_PYTORCH_CPU_MM_Size_" + std::to_string(test_input);
-    benchmark::RegisterBenchmark(name.c_str(), BM_PYTORCH_MM, test_input, torch::kCPU)->UseRealTime()->Unit(benchmark::TimeUnit::kMicrosecond);
+    benchmark::RegisterBenchmark(name.c_str(), BM_PYTORCH_MM, test_input, torch::kCPU)
+        ->UseRealTime()
+        ->Unit(benchmark::TimeUnit::kMicrosecond);
   }
   for (auto& test_input : { 2, 3, 4, 8, 16, 32, 64, 128, 256, 512 })
   {
     std::string name = "BM_PYTORCH_GPU_MM_Size_" + std::to_string(test_input);
-    benchmark::RegisterBenchmark(name.c_str(), BM_PYTORCH_MM, test_input, torch::kCUDA)->UseRealTime()->Unit(benchmark::TimeUnit::kMicrosecond);
+    benchmark::RegisterBenchmark(name.c_str(), BM_PYTORCH_MM, test_input, torch::kCUDA)
+        ->UseRealTime()
+        ->Unit(benchmark::TimeUnit::kMicrosecond);
   }
   for (auto& test_input : { 2, 3, 4, 8, 16, 32, 64, 128, 256, 512 })
   {
     std::string name = "BM_EIGEN_MM_Size_" + std::to_string(test_input);
-    benchmark::RegisterBenchmark(name.c_str(), BM_EIGEN_MM, test_input)->UseRealTime()->Unit(benchmark::TimeUnit::kMicrosecond);
+    benchmark::RegisterBenchmark(name.c_str(), BM_EIGEN_MM, test_input)
+        ->UseRealTime()
+        ->Unit(benchmark::TimeUnit::kMicrosecond);
   }
   for (auto& test_input : { 2, 3, 4, 8, 16, 32, 64, 128, 256, 512 })
   {
     std::string name = "BM_ARRAYFIRE_CPU_MM_Size_" + std::to_string(test_input);
-    benchmark::RegisterBenchmark(name.c_str(), BM_ARRAYFIRE_MM, test_input, AF_BACKEND_CPU)->UseRealTime()->Unit(benchmark::TimeUnit::kMicrosecond);
+    benchmark::RegisterBenchmark(name.c_str(), BM_ARRAYFIRE_MM, test_input, AF_BACKEND_CPU)
+        ->UseRealTime()
+        ->Unit(benchmark::TimeUnit::kMicrosecond);
   }
   for (auto& test_input : { 2, 3, 4, 8, 16, 32, 64, 128, 256, 512 })
   {
     std::string name = "BM_ARRAYFIRE_CUDA_MM_Size_" + std::to_string(test_input);
-    benchmark::RegisterBenchmark(name.c_str(), BM_ARRAYFIRE_MM, test_input, AF_BACKEND_CUDA)->UseRealTime()->Unit(benchmark::TimeUnit::kMicrosecond);
+    benchmark::RegisterBenchmark(name.c_str(), BM_ARRAYFIRE_MM, test_input, AF_BACKEND_CUDA)
+        ->UseRealTime()
+        ->Unit(benchmark::TimeUnit::kMicrosecond);
   }
   for (auto& test_input : { 2, 3, 4, 8, 16, 32, 64, 128, 256, 512 })
   {
     std::string name = "BM_ARRAYFIRE_OPENCL_MM_Size_" + std::to_string(test_input);
-    benchmark::RegisterBenchmark(name.c_str(), BM_ARRAYFIRE_MM, test_input, AF_BACKEND_OPENCL)->UseRealTime()->Unit(benchmark::TimeUnit::kMicrosecond);
+    benchmark::RegisterBenchmark(name.c_str(), BM_ARRAYFIRE_MM, test_input, AF_BACKEND_OPENCL)
+        ->UseRealTime()
+        ->Unit(benchmark::TimeUnit::kMicrosecond);
   }
   benchmark::Initialize(&argc, argv);
   benchmark::RunSpecifiedBenchmarks();
